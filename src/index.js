1a713adc827b361617b2c07cb5c162edb94ab550
Kenjie Tarasona <148972543+Lain-lies@users.noreply.github.com>
2025-09-09 15:15:06 +0800

    WIP: implementing showcase section

--- src/index.js
+++ src/index.js
@@ -8,3 +8,23 @@ window.addEventListener("scroll", () => {
 
   mainBg.style.backgroundPositionX = offset * velocity + "px";
 });
+
+function changeTitleColor() {
+  title;
+  e.target.style.color = "#000";
+}
+
+function revertColor(e) {
+  e.target.style.color = "#fff";
+}
+
+const marketingItem = document.querySelector(".marketing-item");
+const titleFragment = [...document.querySelectorAll(".title > p")];
+
+marketingItem.addEventListener("mouseover", () => {
+  titleFragment.forEach((title) => (title.style.color = "#000"));
+});
+
+marketingItem.addEventListener("mouseleave", () => {
+  titleFragment.forEach((title) => (title.style.color = "#fff"));
+});
